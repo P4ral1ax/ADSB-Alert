b0511528f6d44abac81d6e571f5df4498f5aa746
Brayden <bjw5517@g.rit.edu>
2021-07-19 17:31:06 -0400

    The Fact that this crackpot code works is crazy

--- adsb_make_device.py
+++ adsb_make_device.py
@@ -18,3 +18,18 @@ def make_device(resp):
     device_key  = d.get("device_key")
    
     return(Aircraft(callsign, name, last_time, lat, lon, alt, speed, heading, device_key))
+
+def update_device(device, resp):
+    d = parse_object(resp)
+
+    device.callsign    = d.get("callsign")
+    device.name        = d.get("name")
+    device.last_time   = d.get("last_time")
+    device.lat         = d.get("lat")
+    device.lon         = d.get("lon")
+    device.alt         = d.get("alt")
+    device.speed       = d.get("speed")
+    device.heading     = d.get("heading")
+    device.device_key  = d.get("device_key")
+    
+    return()
--- adsb_parse.py
+++ adsb_parse.py
@@ -11,25 +11,48 @@ def parse_object(resp):
     
     # Create Dictionary to give to dataclass
     device_data = {}
+    try: 
+        # Get All Location Data
+        base_location = data.get("kismet.device.base.location")
+        location_last = base_location.get("kismet.common.location.last")
+        geopoint      = location_last["kismet.common.location.geopoint"]
+        device_data["lat"]      = geopoint[0]
+        device_data["lon"]      = geopoint[1]
+        device_data["alt"]      = location_last["kismet.common.location.alt"]
+        device_data["speed"]    = location_last["kismet.common.location.speed"]
+        device_data["heading"]  = location_last["kismet.common.location.heading"]
 
-    # Get All Location Data
-    base_location = data.get("kismet.device.base.location")
-    location_last = base_location.get("kismet.common.location.last")
-    geopoint      = location_last["kismet.common.location.geopoint"]
-    device_data["lat"]      = geopoint[0]
-    device_data["lon"]      = geopoint[1]
-    device_data["alt"]      = location_last["kismet.common.location.alt"]
-    device_data["speed"]    = location_last["kismet.common.location.speed"]
-    device_data["heading"]  = location_last["kismet.common.location.heading"]
+        # Get Callsign
+        rtladsb_device = data.get("rtladsb.device")
+        device_data["callsign"]     = rtladsb_device["rtladsb.device.callsign"]
+
+        # Get Other Values
+        device_data["name"]         = data.get("kismet.device.base.name")
+        device_data["last_time"]    = data.get("kismet.device.base.last_time")
+        device_data["device_key"]   = data.get("kismet.device.base.key")
+        
+        # Return dictionary
+        return(device_data)
+    except AttributeError:
+        # Missing Data : Ignoring
+        return({})
+
+# Parse the file for all ADS-B devices
+# Return All Devices Kismet IDs in a list (That exist last 10m)
+def parse_all_devices(resp):
+    list_devices = []
+    devices = json.loads(resp.text)
+    for device in devices:
+        list_devices.append(device.get("kismet.device.base.key"))
+    return(list_devices)
+
+
+# Just get the callsign of a device ID
+def get_callsign(resp):
+    data = json.loads(resp.text)
 
     # Get Callsign
     rtladsb_device = data.get("rtladsb.device")
-    device_data["callsign"]     = rtladsb_device["rtladsb.device.callsign"]
+    callsign = rtladsb_device["rtladsb.device.callsign"]
 
-    # Get Other Values
-    device_data["name"]         = data.get("kismet.device.base.name")
-    device_data["last_time"]    = data.get("kismet.device.base.last_time")
-    device_data["device_key"]   = data.get("kismet.device.base.key")
-    
-    # Return to create Dataclass
-    return(device_data)
+    return(callsign)
